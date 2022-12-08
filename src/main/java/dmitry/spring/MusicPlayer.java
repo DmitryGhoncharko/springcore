ef2da5801d0b81915aac2e142eaae5d2075c3bfb
dangeonmaster <dafae722@gmail.com>
2022-12-08 18:39:28 +0300

    spring core 6 less

--- src/main/java/dmitry/spring/MusicPlayer.java
+++ src/main/java/dmitry/spring/MusicPlayer.java
@@ -1,12 +1,55 @@
 package dmitry.spring;
 
+import java.util.List;
+
 public class MusicPlayer {
+
+    private List<String> listMusic;
+    private String name;
+    private int volume;
     private Music music;
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+
+    public List<String> getListMusic() {
+        return listMusic;
+    }
+
+    public void setListMusic(List<String> listMusic) {
+        this.listMusic = listMusic;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
     public void play(){
         System.out.println(music.getSong());
+    }
+    public void playListMusic(){
+        for(String str : listMusic){
+            System.out.println(str);
+        }
     }
 }
--- src/main/java/dmitry/spring/TestSpring.java
+++ src/main/java/dmitry/spring/TestSpring.java
@@ -5,9 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        MusicPlayer musicPlayer = (MusicPlayer) context.getBean("musicPlayer");
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.play();
-
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+        musicPlayer.playListMusic();
         context.close();
     }
 }
